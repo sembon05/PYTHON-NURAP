9a20450ecfc049cea2bb71c7990c19a14c3b47b2
Gustavo Costa <gustavohocosta@gmail.com>
2024-12-05 11:25:59 -0300

    Variaveis e etc

---
+++ aula2.py
@@ -0,0 +1,12 @@
+print('ola mundo')
+print('BOM DIA PRA QUEM É DE BOM DIA')
+
+nome_usuario = 'NJR' # tipo -> str = string
+idade_usuario = 18 # tipo -> int = integer
+altura_usuario = 1.81 # tipo -> = float = flutuante
+maior_idade = True  # tipo -> bool = boolean
+
+print ('Welcome',nome_usuario)
+print ('qual a sua idade?', idade_usuario)
+print ('qual a sua altura?', altura_usuario)
+print ('voce possui a maioridade? ',maior_idade)
